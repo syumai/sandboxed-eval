83d44a53f4e77260c184a458c7b57fe4a8d9c44a
syumai <syumai@gmail.com>
2021-05-31 20:06:07 +0900

    Add id filter

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const handleMessage = (event) => {
   if (event.origin !== origin) {
     return;
   }
-  const { src, scope } = event.data || {};
+  const { id: senderId, src, scope } = event.data || {};
+  if (id !== senderId) {
+    return;
+  }
   const args = Object.keys(scope);
   args.push(src);
   const values = Object.values(scope);
@@ -38,21 +41,21 @@ export function sandboxedEval(src, scope = {}) {
   const iframe = document.createElement("iframe");
   iframe.setAttribute("sandbox", "allow-scripts");
   iframe.setAttribute("style", "display: none;");
-  const msgId = genId();
+  const id = genId();
 
   return new Promise((resolve, reject) => {
     const handleMessage = (event) => {
       if (event.source !== iframe.contentWindow) {
         return;
       }
-      const { id, result, error, ready } = event.data ?? {};
-      if (id !== msgId) {
+      const { id: senderId, result, error, ready } = event.data ?? {};
+      if (id !== senderId) {
         return;
       }
       if (ready) {
         iframe.contentWindow.postMessage(
           {
-            id: msgId,
+            id,
             src,
             scope,
           },
@@ -70,7 +73,7 @@ export function sandboxedEval(src, scope = {}) {
     };
     window.addEventListener("message", handleMessage);
 
-    iframe.srcdoc = srcdoc(window.location.origin, msgId);
+    iframe.srcdoc = srcdoc(window.location.origin, id);
     document.body.appendChild(iframe);
   });
 }
